7e8af2ceaa5d4021ce4e281fe8ee2f9eac6017e0
nilesh0306 <84778457+nilesh0306@users.noreply.github.com>
2024-02-26 21:36:05 +0530

    index.tsx

--- spring-boot-reactjs-todolist/frontend/src/components/header/index.tsx
+++ spring-boot-reactjs-todolist/frontend/src/components/header/index.tsx
@@ -7,13 +7,9 @@ const Header = () => {
     <header className = "p-4 bg-[#1D2A3B]">
       <div className = "flex justify-between items-center mx-auto max-w-6xl px-2 sm:px-6 lg:px-8">
         <a className = "text-2xl text-white font-semibold" href = "#">
-          Listify
+          TODO
         </a>
-        <button 
-          className = "bg-transparent border-2 border-[white] text-white py-2 px-5 text-sm rounded-md font-semibold "
-        >
-          Get App
-        </button>
+      
       </div>
     </header>
   )
